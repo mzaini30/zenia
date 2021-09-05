abc3d0444363a106e4fb56b8a54d63ca91251778
mzaini30 <muhzaini30@gmail.com>
2021-09-06 04:55:04 +0800

    upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import {windi} from 'svelte-windicss-preprocess'
 import adapterVercel from '@sveltejs/adapter-vercel'
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [windi({
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess(), windi({
 		configPath: 'windi.config.cjs'
 	})],
+
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
