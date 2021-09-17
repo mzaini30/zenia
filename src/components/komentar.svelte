04be3b16ebeb74b037819664fcbedbacb2e6d9de
mzaini30 <muhzaini30@gmail.com>
2021-09-17 10:16:55 +0800

    upload

---
+++ src/components/komentar.svelte
@@ -0,0 +1,9 @@
+<!-- <div class="komentar">
+	<form action="">
+		<p>Nama:</p>	
+		<input type="text" class="nama">
+		<p>Komentar:</p>
+		<textarea name="" id="" cols="30" rows="10"></textarea>
+		<input type="submit" value="Kirim Komentar">
+	</form>
+</div> -->
---
+++ src/pages/tes.md
@@ -0,0 +1,8 @@
+---
+layout: ../layouts/blog.astro
+title: Tes
+description: Buat Ngetes
+date: 2021-09-17
+---
+
+Hello
