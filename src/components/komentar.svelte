b5c5d8ad3e54b910d522aecdacfa85e65623c866
mzaini30 <muhzaini30@gmail.com>
2021-09-17 14:33:02 +0800

    oke

--- src/components/komentar.svelte
+++
@@ -1,9 +0,0 @@
-<!-- <div class="komentar">
-	<form action="">
-		<p>Nama:</p>	
-		<input type="text" class="nama">
-		<p>Komentar:</p>
-		<textarea name="" id="" cols="30" rows="10"></textarea>
-		<input type="submit" value="Kirim Komentar">
-	</form>
-</div> -->
